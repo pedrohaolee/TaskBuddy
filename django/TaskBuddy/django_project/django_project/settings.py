6ccbd4222d2c2123afdd1970b90625bbbffb9af1
Hao Li <pedrohaolee@gmail.com>
2024-07-19 14:22:57 +0800

    django setup

--- django/TaskBuddy/django_project/django_project/settings.py
+++ django/TaskBuddy/django_project/django_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first_app',
     'accounts',
     'rest_framework',
     'restful_apis',
---
+++ django/TaskBuddy/django_project/first_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add_member/', views.add_member, name='add_member'),
+    path('update_member/', views.update_member, name='update_member'),
+    path('del_member/', views.del_member, name='del_member'),
+
+    # path('home/', views.home, name='home'),
+    # path('cost/', views.cost, name='cost'),
+]
